c12a70b606b1659f0d7d50dc996c41136718b7c4
Rodrigo Caldeira <rodrigocaldeira@gmail.com>
2020-06-05 22:42:59 -0300

    Adicionando teste de inclusão via API

--- api/api.go
+++ api/api.go
@@ -2,21 +2,57 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/rodrigocaldeira/sistema_facil/database"
 	"github.com/rodrigocaldeira/sistema_facil/estrutura"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type ApiServer struct {
 	Cadastros []*estrutura.Cadastro
+	Database  database.Database
 }
 
 func (server *ApiServer) GetCadastros(w http.ResponseWriter, _ *http.Request) {
 	json.NewEncoder(w).Encode(server.Cadastros)
 }
 
-func InitApi(cadastros []*estrutura.Cadastro) {
-	server := &ApiServer{Cadastros: cadastros}
+func (server *ApiServer) Incluir(w http.ResponseWriter, r *http.Request) {
+	var request map[string]interface{}
+	var valores map[string]interface{}
+
+	var cadastro *estrutura.Cadastro
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Erro ao Incluir", http.StatusBadRequest)
+		return
+	}
+
+	valores = request["__valores"].(map[string]interface{})
+	log.Printf("%+v", valores)
+
+	for _, c := range server.Cadastros {
+		if c.Nome == request["__cadastro"] {
+			cadastro = c
+			break
+		}
+	}
+
+	id, err := server.Database.Incluir(cadastro, valores)
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Erro ao Incluir", http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(strconv.Itoa(id)))
+}
+
+func InitApi(cadastros []*estrutura.Cadastro, db database.Database) {
+	server := &ApiServer{Cadastros: cadastros, Database: db}
 
 	http.HandleFunc("/api/cadastros", server.GetCadastros)
 
--- api/api_test.go
+++ api/api_test.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/rodrigocaldeira/sistema_facil/database"
 	"github.com/rodrigocaldeira/sistema_facil/estrutura"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -21,8 +25,26 @@ func criarCadastros() []*estrutura.Cadastro {
 	return cadastros
 }
 
+var testDB database.Database
+
+func criarDatabase() {
+	caminhoDB, _ := ioutil.TempDir("", "teste_api_sistema_facil")
+	testDB = database.NewTiedotDatabase(caminhoDB)
+	testDB.Iniciar()
+}
+
+func criarServer() *ApiServer {
+	criarDatabase()
+	cadastros := criarCadastros()
+	testDB.Configurar(cadastros[0])
+	server := &ApiServer{Cadastros: cadastros, Database: testDB}
+	return server
+}
+
 func TestGetCadastros(t *testing.T) {
-	server := &ApiServer{Cadastros: criarCadastros()}
+	server := criarServer()
+	defer testDB.Fechar()
+
 	var cadastros []*estrutura.Cadastro
 
 	ts := httptest.NewServer(http.HandlerFunc(server.GetCadastros))
@@ -49,5 +71,55 @@ func TestGetCadastros(t *testing.T) {
 
 	if campoNome.Nome != "Nome" && campoNome.Tipo != "Texto" {
 		t.Error(fmt.Sprintf("Algo muito errado deu no campo do cadastro. Recebi isso: %v", campoNome))
+	}
+}
+
+func TestIncluirCadastro(t *testing.T) {
+	server := criarServer()
+	defer testDB.Fechar()
+
+	cadastro := criarCadastros()[0]
+
+	request, _ := json.Marshal(
+		map[string]interface{}{
+			"__cadastro": "Usuário",
+			"__valores": map[string]interface{}{
+				"nome":  "Rodrigo Caldeira",
+				"email": "rodrigocaldeira@gmail.com",
+			},
+		},
+	)
+
+	ts := httptest.NewServer(http.HandlerFunc(server.Incluir))
+	defer ts.Close()
+
+	res, err := http.Post(ts.URL, "application/json", bytes.NewBuffer(request))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	id, err := strconv.Atoi(string(body))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	usuario, err := testDB.Buscar(cadastro, id)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if usuario["nome"] != "Rodrigo Caldeira" && usuario["email"] != "rodrigocaldeira@gmail.com" {
+		t.Error(fmt.Sprintf("Deu um erro na inclusão do usuário, e salvou isso: %v", usuario))
 	}
 }
